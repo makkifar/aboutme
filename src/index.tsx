b77101d9867c55dec68d1bd80eade8f2ad83bb5d
Farah MAKKI <farah.makki@adidas.com>
2020-06-09 20:28:52 +0200

    test 404 fix

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 import { ApolloProvider } from 'react-apollo'
+import { createBrowserHistory } from 'history'
 
 import App from './App'
 import client from './utils/apolloClient'
@@ -14,5 +15,12 @@ const app = (
     </ApolloProvider>
   </BrowserRouter>
 )
+
+const history = createBrowserHistory()
+
+const path = (/#!(\/.*)$/.exec(location.hash) || [])[1]
+if (path) {
+  history.replace(path)
+}
 
 ReactDOM.render(app, document.getElementById('root'))
